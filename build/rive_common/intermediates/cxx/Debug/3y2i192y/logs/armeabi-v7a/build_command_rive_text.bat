1fdd2c5d2938a37a19f0bd9b7c7c98ab12ea8a16
amirulmukmin3007 <amirulmukmin878@gmail.com>
2025-03-13 09:02:14 +0800

    Removed build folder from tracking

--- build/rive_common/intermediates/cxx/Debug/3y2i192y/logs/armeabi-v7a/build_command_rive_text.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\SDO-MUKMIN\\AppData\\Local\\Android\\sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\SDO-MUKMIN\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\rive_common-0.2.8\\android\\.cxx\\Debug\\3y2i192y\\armeabi-v7a" ^
-  rive_text
